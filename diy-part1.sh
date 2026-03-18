5c2a5ac9db4320bbf04a068b76223a4e77162e0c
lieet <59359907+flrz@users.noreply.github.com>
2026-03-18 21:21:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,24 +12,11 @@
 
 # Uncomment a feed source
 # 建立 OpenClash 內核存放目錄
-mkdir -p package/feeds/openclash/luci-app-openclash/root/etc/openclash/core/
 
-# 設定內核下載地址 (以 x86_64 為例，如果是 ARM 請修改對應名稱)
-# 這裡使用 curl 直接抓取最新 release 的下載連接（簡單做法是指定版本號）
-CORE_VER="v1.18.0" # 建議手動指定一個穩定版本，或使用 API 獲取最新 tag
-DOWNLOAD_URL="https://github.com{CORE_VER}/mihomo-linux-amd64-${CORE_VER}.gz"
-
-# 下載並解壓內核
-curl -fL -o /tmp/clash_meta.gz "$DOWNLOAD_URL"
-gzip -d /tmp/clash_meta.gz
-
-# 移動並更名，同時賦予執行權限
-mv /tmp/clash_meta package/feeds/openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
-chmod +x package/feeds/openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
           
 # Add a feed source
 sed -i '1i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '3i src-git golang https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang' feeds.conf.default
